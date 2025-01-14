8638b6d6d0c1a4ac0354c9705210b970ff86c4a4
Thijs Beumer <t.beumer-1@student.utwente.nl>
2025-01-14 14:55:25 +0100

    Added some README information for initial release

--- README.md
+++ README.md
@@ -1,8 +1,29 @@
-# javascript-modernity
+# JSModernity
 
+A tool to generate modernity signatures for JavaScript projects.
 
-# ECMAScript versions
-- minimal version taken into account: ES3 -> so features from earlier versions are shown as ES3 even though they might be from ES1 for example
+## Installation
+
+- Clone this repository
+- Make sure you are using node version 22.11
+- Run `npm install` to install dependencies
+
+## Running Signature Generation
+
+_This guide is for windows and linux systems only_
+
+- Determine the owner and name of the GitHub repository that you want to generate a signature for
+- Open a terminal
+- Run `node ./run.js --owner=<owner> --repo=<repo>`
+  - You can optionally add a max date parameter `--maxDate=<yyyy-mm-dd>` to prevent any releases after that date from being taken into account
+
+For visualization of the result, you can use the [plots.ipynb](plots.ipynb) file in the root of this repository. It has been created using jupyter notebook version 7.2.2. Other versions might work but have not been tested.
+
+## Important Information
+
+- Only ECMAScript versions ES3 to ES15 are taken into account. Features introduced before ES3 are counted as ES3 features. (This is due to espree supporting only from ES3 onwards)
+- Signatures are generated for every release of a repository, if no releases exist within GitHub, than there will be no signature generated.
 
 ### Known Issues
-- temp_sig_gen_* folders sometimes fail to get deleted after signature generation. After the signature generation is done you can safely delete these folders manually.
+
+- \*temp*sig_gen*\*\* folders sometimes fail to get deleted after signature generation. After the signature generation is done you can safely delete these folders manually.
--- run.js
+++ run.js
@@ -1,6 +1,9 @@
 import { existsSync, mkdirSync, rmSync, writeFileSync } from "fs";
 import { genDirectorySignature } from "./utils/signature-gen-util.js";
-import { getCommitShaByTagName, getReleaseTags } from "./utils/github-api-connector-util.js";
+import {
+  getCommitShaByTagName,
+  getReleaseTags,
+} from "./utils/github-api-connector-util.js";
 import { checkoutCommit, cloneRepo } from "./utils/git-command-util.js";
 import argsParser from "args-parser";
 import { supportedEcmaVersions } from "espree";
@@ -27,7 +30,7 @@ const createEmptyDir = () => {
 
 /**
  * Removes files created during execution time
- * 
+ *
  * @param {number} exitCode exit code to use in process.exit();
  */
 const cleanupAndExit = (exitCode) => {
@@ -35,7 +38,9 @@ const cleanupAndExit = (exitCode) => {
     try {
       rmSync(dir, { recursive: true });
     } catch (e) {
-      console.warn(`Failed to remove temporary files, requires manual deletion... Error: ${e.message}`);
+      console.warn(
+        `Failed to remove temporary files, requires manual deletion... Error: ${e.message}`
+      );
     }
   }
   process.exit(exitCode);
@@ -43,7 +48,7 @@ const cleanupAndExit = (exitCode) => {
 
 /**
  * Creates an empty result object
- * 
+ *
  * @param {object} meta additional meta data to include in the result object
  * @returns {object} a new empty result object
  */
@@ -55,8 +60,8 @@ const createEmptyResultObject = (meta) => {
       signBoolMaxDetectable: getDetectableFeatureCountPerVersion(),
     },
     signatures: {},
-  }
-}
+  };
+};
 
 /**
  * Saves the given result to a file in the result signatures directory.
@@ -66,14 +71,16 @@ const createEmptyResultObject = (meta) => {
 const saveResult = (result) => {
   const dir = RESULT_SIGNATURES_DIR;
   const date = new Date().toISOString();
-  const fileName = `${dir}/signatures_${owner}_${repo}_${date}`;
+  const fileName = `${dir}/${owner}_${repo}_signatures_${date}`;
   const cleanFileName = fileName.replace(/[:\.]/g, "-");
 
   if (!existsSync(dir)) {
     mkdirSync(dir);
   }
 
-  console.log(`Saving result signature for ${owner}:${repo} to file ${fileName}`);
+  console.log(
+    `Saving result signature for ${owner}:${repo} to file ${fileName}`
+  );
 
   writeFileSync(cleanFileName + ".json", JSON.stringify(result, null, 2));
 };
@@ -83,14 +90,14 @@ const timings = { current: performance.now() };
  * Saves the current timing counter as a specific
  * entry in the timings object, and resets the
  * current timer.
- * 
+ *
  * @param {string} key timing key to save time as
  */
 const recordTimingAs = (key) => {
   const accValue = timings[key] ? timings[key] : 0;
   timings[key] = accValue + performance.now() - timings.current;
   timings.current = performance.now();
-}
+};
 
 /**
  * Logs the recorded timings to the console
@@ -98,22 +105,24 @@ const recordTimingAs = (key) => {
 const logTimings = () => {
   const { current, ...finalTimings } = timings;
 
-  const maxKeyLength = Math.max(...Object.keys(finalTimings).map(key => key.length));
+  const maxKeyLength = Math.max(
+    ...Object.keys(finalTimings).map((key) => key.length)
+  );
 
   console.log("------- Timings -------");
   for (const [key, value] of Object.entries(finalTimings)) {
-    const padding = ' '.repeat(maxKeyLength - key.length);
+    const padding = " ".repeat(maxKeyLength - key.length);
     const formattedTiming = msToTime(value);
-    console.log(padding + key + ': ' + formattedTiming);
+    console.log(padding + key + ": " + formattedTiming);
   }
   console.log("-----------------------");
-}
+};
 
 /**
  * Converts the given amount of milliseconds to a
  * days, hours, minutes, seconds and millisceonds
  * format for better readability.
- * 
+ *
  * @param {number} time amount of milliseconds to convert
  * @returns {string} converted representation
  */
@@ -146,29 +155,39 @@ const args = argsParser(process.argv);
 
 // Name of the owner of the repository
 if (!args.owner) {
-  console.error("Error: Missing owner name. Please provide an owner name using the --owner=<NAME> argument...");
+  console.error(
+    "Error: Missing owner name. Please provide an owner name using the --owner=<NAME> argument..."
+  );
   cleanupAndExit(1);
 }
 if (typeof args.owner !== "string") {
-  console.error("Error: Owner name must be a string. Please provide a valid owner name using the --owner=<NAME> argument...");
+  console.error(
+    "Error: Owner name must be a string. Please provide a valid owner name using the --owner=<NAME> argument..."
+  );
   cleanupAndExit(1);
 }
 const owner = args.owner;
 
 // Name of the repository
 if (!args.repo) {
-  console.error("Error: Missing repository name. Please provide a repository name using the --repo=<NAME> argument...");
+  console.error(
+    "Error: Missing repository name. Please provide a repository name using the --repo=<NAME> argument..."
+  );
   cleanupAndExit(1);
 }
 if (typeof args.repo !== "string") {
-  console.error("Error: Repository name must be a string. Please provide a valid repository name using the --repo=<NAME> argument...");
+  console.error(
+    "Error: Repository name must be a string. Please provide a valid repository name using the --repo=<NAME> argument..."
+  );
   cleanupAndExit(1);
 }
 const repo = args.repo;
 
 // Maximum release date
 if (typeof args.maxDate !== "string" || isNaN(Date.parse(args.maxDate))) {
-  console.error("Error: Maximum date must be a string in ISO 8601 format. Please provide a valid maximum date using the --maxDate=<DATE> argument...");
+  console.error(
+    "Error: Maximum date must be a string in ISO 8601 format. Please provide a valid maximum date using the --maxDate=<DATE> argument..."
+  );
   cleanupAndExit(1);
 }
 const maxDate = args.maxDate ? new Date(args.maxDate) : new Date();
@@ -182,17 +201,21 @@ const dir = createEmptyDir();
 
 // Clone the repository
 if (!cloneRepo(dir, owner, repo)) {
-  console.error("Error: Something went wrong while cloning the repository. There is likely additional logging above. Exiting...");
+  console.error(
+    "Error: Something went wrong while cloning the repository. There is likely additional logging above. Exiting..."
+  );
   cleanupAndExit(1);
 }
 
 // Save time taken to clone the repository to timer
-recordTimingAs('Cloning Repository');
+recordTimingAs("Cloning Repository");
 
 // Get all release tags for the repo
 const releaseTags = await getReleaseTags(owner, repo, maxDate.toISOString());
 if (!releaseTags) {
-  console.error("Error: Something went wrong while fetching release tags. There is likely additional logging above. Exiting...");
+  console.error(
+    "Error: Something went wrong while fetching release tags. There is likely additional logging above. Exiting..."
+  );
   cleanupAndExit(1);
 }
 if (releaseTags.length === 0) {
@@ -201,27 +224,42 @@ if (releaseTags.length === 0) {
 }
 
 // Save time taken to fetch release tags to timer
-recordTimingAs('Fetching Release Tags');
+recordTimingAs("Fetching Release Tags");
 
 // Generate signatures for each release tag
-const result = createEmptyResultObject({ owner, repo, maxDate: maxDate.toISOString() });
+const result = createEmptyResultObject({
+  owner,
+  repo,
+  maxDate: maxDate.toISOString(),
+});
 for (const release of releaseTags) {
-
   // Get commit sha for release tag
-  console.log("Fetching commit sha for release tag:", release.tag, "|", release.date);
+  console.log(
+    "Fetching commit sha for release tag:",
+    release.tag,
+    "|",
+    release.date
+  );
   let commitSha = null;
   for (let i = 0; i < MAX_RETRIES; i++) {
     commitSha = await getCommitShaByTagName(owner, repo, release.tag);
     if (commitSha) break;
-    console.warn("Warning: Failed to fetch commit sha for release tag. Retrying... Attempt:", i + 1, "of", MAX_RETRIES);
+    console.warn(
+      "Warning: Failed to fetch commit sha for release tag. Retrying... Attempt:",
+      i + 1,
+      "of",
+      MAX_RETRIES
+    );
   }
   if (!commitSha) {
-    console.error("Error: Something went wrong while fetching the commit sha. There is likely additional logging above. Skipping...");
+    console.error(
+      "Error: Something went wrong while fetching the commit sha. There is likely additional logging above. Skipping..."
+    );
     continue;
   }
 
   // Add time taken to fetch commit sha to timer
-  recordTimingAs('Fetching Commit Sha\'s');
+  recordTimingAs("Fetching Commit Sha's");
 
   // Run 'git checkout' command for commit connected to release tag
   console.log("Running 'git checkout' for commit:", commitSha);
@@ -231,15 +269,22 @@ for (const release of releaseTags) {
       checkoutSuccess = true;
       break;
     }
-    console.warn("Warning: Failed to checkout commit. Retrying... Attempt:", i + 1, "of", MAX_RETRIES);
+    console.warn(
+      "Warning: Failed to checkout commit. Retrying... Attempt:",
+      i + 1,
+      "of",
+      MAX_RETRIES
+    );
   }
   if (!checkoutSuccess) {
-    console.error("Error: Something went wrong while checking out the commit. There is likely additional logging above. Skipping...");
+    console.error(
+      "Error: Something went wrong while checking out the commit. There is likely additional logging above. Skipping..."
+    );
     continue;
   }
 
   // Add time taken to checkout commit to timer
-  recordTimingAs('Checking out commits');
+  recordTimingAs("Checking out commits");
 
   // Generate signature and save to memory
   console.log("Generating signature...");
@@ -247,7 +292,7 @@ for (const release of releaseTags) {
   result.signatures[release.date] = signature;
 
   // Add time taken to generate signature to timer
-  recordTimingAs('Generating signatures');
+  recordTimingAs("Generating signatures");
 }
 
 // Save result to file
